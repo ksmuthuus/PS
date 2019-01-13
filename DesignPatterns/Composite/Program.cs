3d9a357f88cb640c966ddce53185995dba75d445
Muthu Venkatachalam <muthukarupasamy.venkatachalam@honeywell.com>
2019-01-13 13:41:51 +0530

    adding more files

---
+++ DesignPatterns/Composite/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Structural.Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region GrpahicObject
+            var graphics = new GraphicObject { Name = "Graphics" };
+            graphics.Children.Add(new Circle { Color = "Red" });
+            graphics.Children.Add(new Square { Color = "Red" });
+
+            var groups = new GraphicObject();
+            groups.Children.Add(new Circle { Color = "Blue" });
+            groups.Children.Add(new Square { Color = "Blue" });
+            graphics.Children.Add(groups);
+
+            Console.WriteLine(graphics.ToString()); 
+
+            #endregion
+        }
+    }
+}
---
+++ SRP/Journal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRP
+{
+    public class Journal
+    {
+        private readonly List<string> Entries = new List<string>();
+    }
+}
